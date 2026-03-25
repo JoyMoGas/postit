77adb84324572b7a6a6a6178b15b4afab17a6677
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2026-03-24 23:13:47 -0700

    Remove trailing slash from FRONTEND_URL to fix CORS E014 error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,4 +150,4 @@ CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 FRONTEND_URL = os.environ.get("FRONTEND_URL")
 if FRONTEND_URL:
-    CORS_ALLOWED_ORIGINS.append(FRONTEND_URL)
+    CORS_ALLOWED_ORIGINS.append(FRONTEND_URL.rstrip('/'))
